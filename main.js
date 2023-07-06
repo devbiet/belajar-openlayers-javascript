fb71e5214e90cca8912e2afd0b359c123d70073e
DevBiet <dev.biet@outlook.com>
2023-07-06 08:16:10 +0700

    add popup

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 import Map from "ol/Map";
+import Overlay from "ol/Overlay";
 import View from "ol/View";
 import MousePosition from "ol/control/MousePosition";
 import { format } from "ol/coordinate";
 import ScaleLine from "ol/control/ScaleLine";
 import TileLayer from "ol/layer/Tile";
-import { fromLonLat } from 'ol/proj';
+import { fromLonLat, toLonLat } from 'ol/proj';
 import OSM from "ol/source/OSM";
 
 import "ol/ol.css";
@@ -43,3 +44,31 @@ const scaleControl = new ScaleLine({
 });
 
 map.addControl(scaleControl);
+
+const container = document.getElementById('popup');
+const content = document.getElementById('popup-content');
+const closer = document.getElementById('popup-closer');
+
+const popup = new Overlay({
+  element: container,
+  autoPan: true,
+  autoPanAnimation: {
+    duration: 250,
+  },
+});
+
+map.addOverlay(popup);
+
+closer.onclick = function () {
+  popup.setPosition(undefined);
+  closer.blur();
+  return false;
+};
+
+map.on('singleclick', function (evt) {
+  const coordinate = evt.coordinate;
+  const hdms = toLonLat(coordinate);
+
+  content.innerHTML = '<p>You clicked here:</p><code>' + hdms + '</code>';
+  popup.setPosition(coordinate);
+});
